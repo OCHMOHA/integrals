0c2b52f2a57674ec921dd4dfca0dfd5a07f87151
OCHMOHA <1132245115@pfur.ru>
2025-05-27 09:47:13 +0300

    Update zad_5.cpp

---
+++ zad_5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double func(double x) {
+    double denom = x * sqrt(3 * x * x - 2 * x - 1);
+    return 1.0 / denom;
+}
+
+double simpson(double (*f)(double), double a, double b, int nseg) {
+    if (nseg % 2 == 1) nseg++;
+    double h = (b - a) / nseg;
+    double result = f(a) + f(b);
+    for (int i = 1; i < nseg; i += 2)
+        result += 4 * f(a + i * h);
+    for (int i = 2; i < nseg; i += 2)
+        result += 2 * f(a + i * h);
+    return result * h / 3.0;
+}
+
+int main() {
+    double a = 1, b = 2;
+    double eps = 1e-7;
+    int nseg = 2;
+
+    double int1 = simpson(func, a, b, nseg);
+    double int2 = simpson(func, a, b, nseg * 2);
+
+    while (fabs(int1 - int2) > eps) {
+        nseg *= 2;
+        int1 = simpson(func, a, b, nseg);
+        int2 = simpson(func, a, b, nseg * 2);
+    }
+
+    cout << "Integral 5 Result: " << int2 << endl;
+    cout << "Segments used: " << nseg * 2 << endl;
+    return 0;
+}
