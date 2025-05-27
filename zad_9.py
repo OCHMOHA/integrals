423e4b8bbef72eeca105b0e729d3cf96a5ee5e0f
OCHMOHA <1132245115@pfur.ru>
2025-05-27 06:57:50 +0300

    Update zad_9.py

---
+++ zad_9.py
@@ -0,0 +1,33 @@
+from math import exp, cos
+
+def simpson_rule(func, a, b, nseg):
+    if nseg % 2 == 1:
+        nseg += 1
+    dx = (b - a) / nseg
+    total = func(a) + func(b)
+    for i in range(1, nseg, 2):
+        total += 4 * func(a + i * dx)
+    for i in range(2, nseg, 2):
+        total += 2 * func(a + i * dx)
+    return total * dx / 3
+
+def func(x):
+    return exp(-x) * cos(x)
+
+print("Using Simpson's method")
+print("Integrating function: f(x) = e^(-x) * cos(x)")
+
+a = 0
+b = 100
+nseg = 2
+eps = 1e-7
+
+int_1 = simpson_rule(func, a, b, nseg)
+int_2 = simpson_rule(func, a, b, nseg * 2)
+
+while abs(int_1 - int_2) > eps:
+    nseg *= 2
+    int_1 = simpson_rule(func, a, b, nseg)
+    int_2 = simpson_rule(func, a, b, nseg * 2)
+
+print("\nAnswer: I =", int_2, "\nNumber of segments:", nseg * 2)
