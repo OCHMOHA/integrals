01364526033d48362b1a5fa8e212d4c6c2365fce
OCHMOHA <1132245115@pfur.ru>
2025-05-27 06:56:36 +0300

    Update zad_6.py

---
+++ zad_6.py
@@ -0,0 +1,33 @@
+from math import sqrt
+
+def simpson_rule(func, a, b, nseg):
+    if nseg % 2 == 1:
+        nseg += 1
+    dx = (b - a) / nseg
+    total = func(a) + func(b)
+    for i in range(1, nseg, 2):
+        total += 4 * func(a + i * dx)
+    for i in range(2, nseg, 2):
+        total += 2 * func(a + i * dx)
+    return total * dx / 3
+
+def func(x):
+    return 1 / ((1 + x) * sqrt(x))
+
+print("Using Simpson's method")
+print("Integrating function: f(x) = 1 / ((1 + x) * sqrt(x))")
+
+a = 1
+b = 1000
+nseg = 2
+eps = 1e-7
+
+int_1 = simpson_rule(func, a, b, nseg)
+int_2 = simpson_rule(func, a, b, nseg * 2)
+
+while abs(int_1 - int_2) > eps:
+    nseg *= 2
+    int_1 = simpson_rule(func, a, b, nseg)
+    int_2 = simpson_rule(func, a, b, nseg * 2)
+
+print("\nAnswer: I =", int_2, "\nNumber of segments:", nseg * 2)
