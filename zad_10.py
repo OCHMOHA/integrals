a6cb9c29245e76cdd043d6c7aa6e10835a59c155
OCHMOHA <1132245115@pfur.ru>
2025-05-27 06:58:22 +0300

    Update zad_10.py

---
+++ zad_10.py
@@ -0,0 +1,31 @@
+def simpson_rule(func, a, b, nseg):
+    if nseg % 2 == 1:
+        nseg += 1
+    dx = (b - a) / nseg
+    total = func(a) + func(b)
+    for i in range(1, nseg, 2):
+        total += 4 * func(a + i * dx)
+    for i in range(2, nseg, 2):
+        total += 2 * func(a + i * dx)
+    return total * dx / 3
+
+def func(x):
+    return 1 / (x**2 - 4*x + 3)
+
+print("Using Simpson's method")
+print("Integrating function: f(x) = 1 / (x^2 - 4x + 3)")
+
+a = 0
+b = 2 - 1e-6
+nseg = 2
+eps = 1e-7
+
+int_1 = simpson_rule(func, a, b, nseg)
+int_2 = simpson_rule(func, a, b, nseg * 2)
+
+while abs(int_1 - int_2) > eps:
+    nseg *= 2
+    int_1 = simpson_rule(func, a, b, nseg)
+    int_2 = simpson_rule(func, a, b, nseg * 2)
+
+print("\nAnswer: I =", int_2, "\nNumber of segments:", nseg * 2)
